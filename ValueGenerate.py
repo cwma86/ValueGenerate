8b01bc55c42657e7b815c584778ecceff1dd05d6
cwma86 <36380847+cwma86@users.noreply.github.com>
2019-10-19 15:21:40 -0600

    update for publishing all position data

--- ValueGenerate.py
+++ ValueGenerate.py
@@ -3,7 +3,6 @@ import argparse
 import socket
 
 
-
 class options():
     def __init__(self):
         parser = argparse.ArgumentParser(description='creates and publishes position data')
@@ -25,34 +24,34 @@ class velocity:
         self.y = velocityY
         self.z = velocityZ
 
-def main():
-    inputArgs = options()
+def sendData(data):
     host_ip, server_port = "127.0.0.1", 9999
-    data = "position data"
-    initPosition = position(1, 0, 0, 0)
-
-    # TODO make the convert to string a class method
-    data = str(initPosition.x) + " " + str(initPosition.y) + " " + \
-            str(initPosition.z) + " " + str(initPosition.time)
-    # TODO establish connection
+    # Establish connection
     tcp_client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     try:
         tcp_client.connect((host_ip, server_port))
-        # TODO populate data with the simulated positon
+        # Populate data with the simulated positon
         tcp_client.sendall(bytes(data + "\n" , "ASCII"))
 
         received = tcp_client.recv(1024)
+
+         
         # TODO do i need error handling for missed response? 
     finally:
         tcp_client.close()
     print("sent : {}".format(data))
     print("received: {}".format(received.decode))
-    # TODO Publish data via TCP
 
+
+def main():
+    # TODO input args for host/port
+    inputArgs = options()
+    initPosition = position(1, 0, 0, 0)
     initVelocity = velocity(1,0,0) 
     startTime = initPosition.time
-    endTime = 100
+    endTime = 10
     newPosition = initPosition
+
     for time in range(startTime, endTime):
         timeDelta = time - initPosition.time
         newPosition.x = initPosition.x + initVelocity.x * timeDelta 
@@ -62,8 +61,11 @@ def main():
         print("position x = %s y = %s z = %s at time = %s" %(newPosition.x,
                                                  newPosition.y, newPosition.z, 
                                                  newPosition.time))
-        # TODO publish next position
-
+        # TODO create convert class attributes to string method
+        data = str(newPosition.x) + " " + str(newPosition.y) + " " + \
+                str(newPosition.z) + " " + str(newPosition.time)
+        # Publish position data via TCP
+        sendData(data)
         initPosition = newPosition
 
 if __name__ == "__main__":
